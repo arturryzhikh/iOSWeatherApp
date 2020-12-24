1b4dfbe0d6fe32c6069674a253b3af70ea1f38c8
arturryzhikh <ryzhiha@gmail.com>
2020-12-24 13:07:39 +0300

    move back collecting view delegate into weather controller ))

--- iOSWeatherApp/Screens/Weather/DataSource/DataSource.swift
+++ iOSWeatherApp/Screens/Weather/DataSource/DataSource.swift
@@ -10,14 +10,10 @@ import UIKit
 final class DataSource: NSObject  {
     
     private var collectionView: UICollectionView!
-    private var width: CGFloat {
-        return collectionView.frame.width
-    }
     init(of collectionView: UICollectionView) {
         super.init()
         self.collectionView = collectionView
         collectionView.dataSource = self
-        collectionView.delegate = self
         collectionView.registerHeaders(TodayHeader.self)
         collectionView.registerFooters(TodayFooter.self)
         collectionView.registerCells(DailyWeatherCell.self,
@@ -95,45 +91,3 @@ extension DataSource: UICollectionViewDataSource {
         }
     }
  }
-//MARK: UICollectionViewDelegateFlowLayout
-extension DataSource: UICollectionViewDelegateFlowLayout {
-    //item size
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let section = indexPath.section
-        let partialWidth = width * 0.9
-        let height = Screen.height
-        switch section {
-        case 1:
-            return CGSize(width: partialWidth , height: (height * 0.066))
-        case 2:
-            return CGSize(width: partialWidth, height: (height * 0.131))
-        case 3:
-            return CGSize(width: partialWidth, height: (height * 0.07))
-        case 4:
-            return CGSize(width: width, height: (height * 0.09))
-        default:
-            return .zero
-        }
-    }
-    //header size
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        referenceSizeForHeaderInSection section: Int) -> CGSize {
-        let headerSize = CGSize(width: width, height: TodayHeader.defaultHeight)
-        return section == 0 ? headerSize : .zero
-    }
-    //footer size
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        referenceSizeForFooterInSection section: Int) -> CGSize {
-        let footerSize =  CGSize(width: width, height: TodayFooter.defaultHeight)
-        return section == 0 ? footerSize : .zero
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
-                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return .zero
-    }
-    
-}
--- iOSWeatherApp/Screens/Weather/Views/WeatherController.swift
+++ iOSWeatherApp/Screens/Weather/Views/WeatherController.swift
@@ -12,6 +12,11 @@ final class WeatherController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+    //MARK: Other Properties
+    private var width: CGFloat {
+        return collectionView.frame.width
+    }
+   
     //MARK: Subviews
     private var weatherView: WeatherView {
         return self.view as! WeatherView
@@ -27,9 +32,52 @@ final class WeatherController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
         dataProvider = DataSource(of: collectionView)
         
         
+    }
+    
+}
+//MARK: UICollectionViewDelegateFlowLayout
+extension WeatherController: UICollectionViewDelegateFlowLayout {
+    //item size
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let section = indexPath.section
+        let partialWidth = width * 0.9
+        let height = Screen.height
+        switch section {
+        case 1:
+            return CGSize(width: partialWidth , height: (height * 0.066))
+        case 2:
+            return CGSize(width: partialWidth, height: (height * 0.131))
+        case 3:
+            return CGSize(width: partialWidth, height: (height * 0.07))
+        case 4:
+            return CGSize(width: width, height: (height * 0.09))
+        default:
+            return .zero
+        }
+    }
+    //header size
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let headerSize = CGSize(width: width, height: TodayHeader.defaultHeight)
+        return section == 0 ? headerSize : .zero
+    }
+    //footer size
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForFooterInSection section: Int) -> CGSize {
+        let footerSize =  CGSize(width: width, height: TodayFooter.defaultHeight)
+        return section == 0 ? footerSize : .zero
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
     }
     
 }
